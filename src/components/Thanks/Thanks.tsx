19d147670ce36279a97f3cf8d690ee983553f785
Bumble <bumble.sakh@gmail.com>
2023-04-22 19:22:03 +1100

    feat: add thanks card

--- src/components/Thanks/Thanks.tsx
+++ src/components/Thanks/Thanks.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react';
 import styles from './Thanks.module.scss';
+import image from '../../assets/images/illustration-thank-you.svg';
 
 type PropsType = {
   rating: number | null;
@@ -7,13 +8,14 @@ type PropsType = {
 
 const Thanks: FC<PropsType> = ({ rating }) => {
   return (
-    <div className={styles.thanks}>
-      <p>You selected {rating} out of 5</p>
-      <p> Thank you!</p>
-      <p>
-        We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in
-        touch!
-      </p>
+    <div className={styles.card}>
+      <div className={styles.image}>
+        <img src={image} alt='Illustration' />
+      </div>
+      <p className={styles.rating}>You selected {rating} out of 5</p>
+      <h1 className={styles.title}> Thank you!</h1>
+      <p className={styles.subtitle}>We appreciate you taking the time to give a rating.</p>
+      <p className={styles.text}>If you ever need more support, don’t hesitate to get in touch!</p>
     </div>
   );
 };
